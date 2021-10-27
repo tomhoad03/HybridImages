6a180af62e5af604358563ddc2516eb1b91e1044
Tom Hoad <40165392+tomhoad03@users.noreply.github.com>
2021-10-27 16:22:30 +0100

    Convolution algorithm

--- HybridImages/src/main/java/uk/ac/soton/ecs/tdh1g19/hybridimages/MyConvolution.java
+++ HybridImages/src/main/java/uk/ac/soton/ecs/tdh1g19/hybridimages/MyConvolution.java
@@ -4,18 +4,42 @@ import org.openimaj.image.FImage;
 import org.openimaj.image.processor.SinglebandImageProcessor;
 
 public class MyConvolution implements SinglebandImageProcessor<Float, FImage> {
-    private float[][] kernel;
+    private final float[][] kernel;
 
     public MyConvolution(float[][] kernel) {
-        //note that like the image pixels kernel is indexed by [row][column]
         this.kernel = kernel;
     }
 
     @Override
     public void processImage(FImage image) {
-        // convolve image with kernel and store result back in image
-        //
-        // hint: use FImage#internalAssign(FImage) to set the contents
-        // of your temporary buffer image to the image.
+        int kernelHeight = kernel.length;
+        int kernelWidth = kernel[0].length;
+
+        int imageHeight = image.getHeight();
+        int imageWidth = image.getWidth();
+
+        FImage original = image.internalAssign(image);
+
+        // search for pixels not at the boundary
+        for (int i = 0; i < imageHeight; i++) {
+            for (int j = 0; j < imageWidth; j++) {
+                float result = 0;
+                int p = 0;
+
+                for (int m = j - ((kernelWidth - 1) / 2); m < j + ((kernelWidth - 1) / 2) - 1; m++) {
+                    int q = 0;
+                    for (int n = i - ((kernelHeight - 1) / 2); n < i + ((kernelHeight - 1) / 2) - 1; n++) {
+                        if (m >= 0 && n >= 0 && m < imageWidth && n < imageHeight) {
+                            float template = kernel[p][q];
+                            float pixel = original.getPixel(m, n);
+                            result += template * pixel;
+                        }
+                        q++;
+                    }
+                    p++;
+                }
+                image.setPixel(j, i, result);
+            }
+        }
     }
 }
--- HybridImages/src/main/java/uk/ac/soton/ecs/tdh1g19/hybridimages/MyHybridImages.java
+++ HybridImages/src/main/java/uk/ac/soton/ecs/tdh1g19/hybridimages/MyHybridImages.java
@@ -1,6 +1,11 @@
 package uk.ac.soton.ecs.tdh1g19.hybridimages;
 
+import org.openimaj.image.DisplayUtilities;
+import org.openimaj.image.ImageUtilities;
 import org.openimaj.image.MBFImage;
+
+import java.io.File;
+import java.util.Objects;
 
 public class MyHybridImages {
     /**
@@ -18,15 +23,42 @@ public class MyHybridImages {
      * @return the computed hybrid image
      */
     public static MBFImage makeHybrid(MBFImage lowImage, float lowSigma, MBFImage highImage, float highSigma) {
-        //implement your hybrid images functionality here.
-        //Your submitted code must contain this method, but you can add
-        //additional static methods or implement the functionality through
-        //instance methods on the `MyHybridImages` class of which you can create
-        //an instance of here if you so wish.
-        //Note that the input images are expected to have the same size, and the output
-        //image will also have the same height & width as the inputs.
-        return null;
+        try {
+            // Applies a low pass filter by convolution
+            int lowPassSize = (int) (8.0f * lowSigma + 1.0f);
+            if (lowPassSize % 2 == 0) lowPassSize++;
+            float[][] lowPassKernel = new float[lowPassSize][lowPassSize];
+            lowImage.processInplace(new MyConvolution(lowPassKernel));
+
+            // Applies a high pass filter by convolution
+            int highPassSize = (int) (8.0f * highSigma + 1.0f);
+            if (highPassSize % 2 == 0) highPassSize++;
+            float[][] highPassKernel = new float[highPassSize][highPassSize];
+            // highImage.processInplace(new MyConvolution(highPassKernel));
+
+            return lowImage;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
-    // java -jar SubmissionTester.jar MyConvolution.java MyHybridImages.java
+    public static void main(String[] args) {
+        try {
+            MBFImage catImage = ImageUtilities.readMBF(new File("D:\\Documents\\Coursework2\\HybridImages\\src\\main\\java\\uk\\ac\\soton\\ecs\\tdh1g19\\hybridimages\\images\\cat.bmp"));
+            MBFImage dogImage = ImageUtilities.readMBF(new File("D:\\Documents\\Coursework2\\HybridImages\\src\\main\\java\\uk\\ac\\soton\\ecs\\tdh1g19\\hybridimages\\images\\dog.bmp"));
+
+            DisplayUtilities.display(Objects.requireNonNull(makeHybrid(catImage, 1, dogImage, 1)));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*
+      To do:
+      1. Create a convolution algorithm
+      2. Apply a low pass filter to an image
+      3. Apply a high pass filter to an image
+      4. Sum two images together
+     */
 }
