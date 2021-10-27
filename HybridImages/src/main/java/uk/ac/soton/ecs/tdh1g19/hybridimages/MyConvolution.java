8cab4dd10894aff1b5d607f8f6ad29d3fd167760
Tom Hoad <40165392+tomhoad03@users.noreply.github.com>
2021-10-27 13:12:24 +0100

    Setting up Maven project, configuration and skeleton classes

---
+++ HybridImages/src/main/java/uk/ac/soton/ecs/tdh1g19/hybridimages/MyConvolution.java
@@ -0,0 +1,21 @@
+package uk.ac.soton.ecs.tdh1g19.hybridimages;
+
+import org.openimaj.image.FImage;
+import org.openimaj.image.processor.SinglebandImageProcessor;
+
+public class MyConvolution implements SinglebandImageProcessor<Float, FImage> {
+    private float[][] kernel;
+
+    public MyConvolution(float[][] kernel) {
+        //note that like the image pixels kernel is indexed by [row][column]
+        this.kernel = kernel;
+    }
+
+    @Override
+    public void processImage(FImage image) {
+        // convolve image with kernel and store result back in image
+        //
+        // hint: use FImage#internalAssign(FImage) to set the contents
+        // of your temporary buffer image to the image.
+    }
+}
