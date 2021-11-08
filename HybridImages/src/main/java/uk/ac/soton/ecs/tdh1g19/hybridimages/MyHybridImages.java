712ad02beefcd1ee9c76ff4558a0b13c6be032bf
Tom Hoad <40165392+tomhoad03@users.noreply.github.com>
2021-11-08 16:20:54 +0000

    Finishing the hybrid images coursework

--- HybridImages/src/main/java/uk/ac/soton/ecs/tdh1g19/hybridimages/MyHybridImages.java
+++ HybridImages/src/main/java/uk/ac/soton/ecs/tdh1g19/hybridimages/MyHybridImages.java
@@ -1,12 +1,18 @@
 package uk.ac.soton.ecs.tdh1g19.hybridimages;
 
+import org.checkerframework.checker.units.qual.A;
 import org.openimaj.image.DisplayUtilities;
 import org.openimaj.image.FImage;
 import org.openimaj.image.ImageUtilities;
 import org.openimaj.image.MBFImage;
+import org.openimaj.image.colour.ColourSpace;
 import org.openimaj.image.processing.convolution.Gaussian2D;
+import org.openimaj.image.processing.resize.ResizeProcessor;
 
+import javax.swing.*;
+import java.awt.*;
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class MyHybridImages {
@@ -52,22 +58,27 @@ public class MyHybridImages {
         }
     }
 
-    public static void main(String[] args) {
+    public static void customHybrid() {
         try {
-            MBFImage catImage = ImageUtilities.readMBF(new File("D:\\Documents\\Coursework2\\HybridImages\\src\\main\\java\\uk\\ac\\soton\\ecs\\tdh1g19\\hybridimages\\images\\cat.bmp"));
-            MBFImage dogImage = ImageUtilities.readMBF(new File("D:\\Documents\\Coursework2\\HybridImages\\src\\main\\java\\uk\\ac\\soton\\ecs\\tdh1g19\\hybridimages\\images\\dog.bmp"));
+            MBFImage imageA = ImageUtilities.readMBF(new File("D:\\Documents\\Coursework2\\HybridImages\\src\\main\\java\\uk\\ac\\soton\\ecs\\tdh1g19\\hybridimages\\images\\johnson.bmp"));
+            MBFImage imageB = ImageUtilities.readMBF(new File("D:\\Documents\\Coursework2\\HybridImages\\src\\main\\java\\uk\\ac\\soton\\ecs\\tdh1g19\\hybridimages\\images\\worm.bmp"));
+            MBFImage blank = ImageUtilities.readMBF(new File("D:\\Documents\\Coursework2\\HybridImages\\src\\main\\java\\uk\\ac\\soton\\ecs\\tdh1g19\\hybridimages\\images\\blank.bmp"));
 
-            DisplayUtilities.display(Objects.requireNonNull(makeHybrid(dogImage, 5, catImage, 5)));
+            MBFImage hybrid = makeHybrid(imageA, 12, imageB, 12);
+
+            blank.drawImage(hybrid, 0, 0);
+            blank.drawImage(ResizeProcessor.halfSize(Objects.requireNonNull(hybrid).clone()), 532, 256);
+            blank.drawImage(ResizeProcessor.halfSize(ResizeProcessor.halfSize(hybrid.clone())), 808, 384);
+            blank.drawImage(ResizeProcessor.halfSize(ResizeProcessor.halfSize(ResizeProcessor.halfSize(hybrid.clone()))), 956, 448);
+            blank.drawImage(ResizeProcessor.halfSize(ResizeProcessor.halfSize(ResizeProcessor.halfSize(ResizeProcessor.halfSize(hybrid.clone())))), 1040, 480);
+
+            DisplayUtilities.display(blank);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    /*
-      To do:
-      1. Create a convolution algorithm
-      2. Apply a low pass filter to an image
-      3. Apply a high pass filter to an image
-      4. Sum two images together
-     */
+    public static void main(String[] args) {
+        customHybrid();
+    }
 }
